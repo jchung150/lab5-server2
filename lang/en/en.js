c7e4cdd0d13572e43bb92426227fd4a8e37df0c3
jchung150 <cloudfan150@gmail.com>
2024-10-09 22:22:30 -0700

    Removed lang folder

--- lang/en/en.js
+++
@@ -1,9 +0,0 @@
-const userStrings = {
-    notFound: "Word '{term}' not found.",
-    endpointNotFound: "Endpoint not found.",
-    wordExists: "Word already exists in the dictionary.",
-    invalidData: "Invalid data. Both word and definition are required.",
-    invalidJSON: "Invalid JSON data."
-  };
-  
-  module.exports = userStrings;
